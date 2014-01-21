f649acedbf06ee38b5d1255469a7ae2e57176102
Thijs Kanters <aelse.lb@gmail.com>
2014-01-21 08:05:04 +0100

    Update temperature.py

--- code/temperature.py
+++ code/temperature.py
@@ -4,10 +4,10 @@ import time
 import subprocess
 import datetime
 
-dataFile_sensor1 = open('/home/thijs/HAB/code/results/inside.csv', 'a') #creating sensor1.txt
-dataFile_sensor2 = open('/home/thijs/HAB/code/results/outside.csv', 'a')
+dataFile_sensor1 = open('/home/thijs/HAB/code/results/inside.csv', 'a') #creating sensor1.csv
+dataFile_sensor2 = open('/home/thijs/HAB/code/results/outside.csv', 'a') #creating sensor1.csv
 os.system('modprobe w1-gpio') #Initializing the sensors
-os.system('modprobe w1-therm') 
+os.system('modprobe w1-therm') #Initializing the sensors
 
 #Sensor 28-0000052c5567
 base_dir_sensor1 = '/sys/bus/w1/devices/'
@@ -18,7 +18,7 @@ base_dir_sensor2 = '/sys/bus/w1/devices/'
 device_folder_sensor2 = glob.glob(base_dir_sensor2 + '28-0000052cc99d')[0]
 device_file_sensor2 = device_folder_sensor2 + '/w1_slave'
 
- 
+
 def read_temp_raw_sensor1():
 	catdata = subprocess.Popen(['cat',device_file_sensor1], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	out,err = catdata.communicate()
@@ -56,13 +56,13 @@ def read_temp_sensor2():
         if equals_pos != -1:
                 temp_string = lines[1][equals_pos+2:]
                 temp_c = float(temp_string) / 1000.0
-                dataFile_sensor2.write(str(temp_c)+ ","+ str(datetime.datetime.now())+'\n')
+                dataFile_sensor2.write(str(temp_c)+ ","+ str(datetime.datetime.now())+'\n') #output format in sensor1.csv
                 return temp_c
 
 	
 while True:
         read_temp_sensor1()
         read_temp_sensor2()
-        dataFile_sensor1.close()
-        dataFile_sensor2.close()
+        dataFile_sensor1.close() #closing the file
+        dataFile_sensor2.close() #closing the file
         time.sleep(300) #sleep time in seconds
